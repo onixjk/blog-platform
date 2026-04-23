81deed3f7bc0b995f18893bc08468013576464d1
Alexander <onixjkk@gmail.com>
2026-04-23 18:46:32 +0300

    fix

--- src/blog/routers/handlers/get-blog-list.handler.ts
+++ src/blog/routers/handlers/get-blog-list.handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import {blogRepository} from "../../repositories/blog.repository";
 
-export function getDriverListHandler(req: Request, res: Response) {
+export function getBlogListHandler(req: Request, res: Response) {
     const blogs = blogRepository.findAll();
 
     res.send(blogs);
