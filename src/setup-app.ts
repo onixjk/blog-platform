5a3b078922b0b475bfb02f8094b5b36ed3df5875
Alexander <onixjkk@gmail.com>
2026-04-24 16:39:56 +0300

    Add router

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,12 +1,14 @@
 
 import express, { Express } from "express";
-import {BLOGS_PATH, TESTING_PATH} from "./core/paths/paths";
+import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/paths/paths";
 import {blogRouter} from "./blog/routers/blog.router";
 import {testingRouter} from "./testing/routers/testing.router";
+import {postRouter} from "./post/routers/post.routers";
 
 export const setupApp = (app: Express) => {
     app.use(express.json());
     app.use(BLOGS_PATH, blogRouter)
+    app.use(POSTS_PATH, postRouter)
     app.use(TESTING_PATH, testingRouter)
 
     return app;
