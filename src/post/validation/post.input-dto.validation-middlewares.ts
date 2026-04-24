8c3aae6528fc15add12ef341e2c6ee8b40f9ca64
Alexander <onixjkk@gmail.com>
2026-04-24 16:23:17 +0300

    Add post.input-dto.validation-middlewares.ts

---
+++ src/post/validation/post.input-dto.validation-middlewares.ts
@@ -0,0 +1,29 @@
+import {body} from "express-validator";
+
+const titleValidation = body('title')
+    .isString().withMessage('title should be string')
+    .trim()
+    .isLength({max: 30})
+    .withMessage('Length of title is not correct');
+
+const shortDescriptionValidation = body('shortDescription')
+    .isString().withMessage('ShortDescription should be string')
+    .isLength({max: 100})
+    .withMessage('Length of shortDescription is not correct');
+
+const contentValidation = body('content')
+    .isString().withMessage('Content should be string')
+    .isLength({max: 1000})
+    .withMessage('Length of content is not correct');
+
+const blogIdValidation = body('blogId')
+    .isString().withMessage('BlogId should be string')
+    .isLength({min: 1})
+    .withMessage('Length of blogId is not correct');
+
+export const postInputDtoValidation = [
+    titleValidation,
+    shortDescriptionValidation,
+    contentValidation,
+    blogIdValidation,
+];
