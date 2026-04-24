a13af373a0b2c7b40177df9eb0aa187cb598cfff
Alexander <onixjkk@gmail.com>
2026-04-24 16:02:04 +0300

    Add delete-post.handler.ts

---
+++ src/post/repositories/handlers/delete-post.handler.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from 'express';
+import {HttpStatus} from "../../../core/types/http-statuses";
+import {createErrorsMessages} from "../../../core/utils/error.utils";
+import {postRepository} from "../post.repository";
+
+export function deletePostHandler(req: Request, res: Response) {
+    const id = req.params.id.toString();
+    const post = postRepository.findById(id);
+
+    if (!post) {
+        res
+            .status(HttpStatus.NotFound_404)
+            .send(createErrorsMessages([{message: "Post not found", field: "id"}]));
+        return;
+    }
+
+    postRepository.delete(id);
+    res.sendStatus(HttpStatus.NoContent_204);
+}
