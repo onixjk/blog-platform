2684074bbd8d5734322932353e0179779014577c
Alexander <onixjkk@gmail.com>
2026-04-25 16:12:34 +0300

    fix

--- src/post/validation/post.input-dto.validation-middlewares.ts
+++ src/post/validation/post.input-dto.validation-middlewares.ts
@@ -3,7 +3,7 @@ import {body} from "express-validator";
 const titleValidation = body('title')
     .isString().withMessage('title should be string')
     .trim()
-    .isLength({max: 30})
+    .isLength({min:1, max: 30})
     .withMessage('Length of title is not correct');
 
 const shortDescriptionValidation = body('shortDescription')
@@ -13,11 +13,12 @@ const shortDescriptionValidation = body('shortDescription')
 
 const contentValidation = body('content')
     .isString().withMessage('Content should be string')
-    .isLength({max: 1000})
+    .isLength({min:1, max: 1000})
     .withMessage('Length of content is not correct');
 
 const blogIdValidation = body('blogId')
     .isString().withMessage('BlogId should be string')
+    .trim()
     .isLength({min: 1})
     .withMessage('Length of blogId is not correct');
 
