5c93ce5c599aac6a62b61f01e5158432fe58ffa7
Alexander <onixjkk@gmail.com>
2026-04-25 17:47:39 +0300

    fix

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -4,8 +4,11 @@ import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/paths/paths";
 import {blogRouter} from "./blog/routers/blog.router";
 import {testingRouter} from "./testing/routers/testing.router";
 import {postRouter} from "./post/routers/post.routers";
+import cors from 'cors';
 
 export const setupApp = (app: Express) => {
+
+    app.use(cors());
     app.use(express.json());
     app.use(BLOGS_PATH, blogRouter)
     app.use(POSTS_PATH, postRouter)
