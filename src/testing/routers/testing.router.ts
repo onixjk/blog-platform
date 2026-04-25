94500c496fefd8cd2f2d480d73c9e2d85b8d9606
Alexander <onixjkk@gmail.com>
2026-04-25 11:23:39 +0300

    fix

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -4,7 +4,7 @@ import {HttpStatus} from "../../core/types/http-statuses";
 
 export const testingRouter = Router();
 
-testingRouter.delete('all-data', (req: Request, res: Response) => {
+testingRouter.delete('/all-data', (req: Request, res: Response) => {
     db.blogs = [];
     db.posts = [];
     res.sendStatus(HttpStatus.NoContent_204)
