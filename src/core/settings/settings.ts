678b3d0b3487f925f11087b7fd64a6bd60cb7113
Alexander <onixjkk@gmail.com>
2026-04-27 23:12:53 +0300

    fix

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,6 +1,7 @@
 export const SETTINGS = {
     PORT: process.env.PORT || 5001,
     MONGO_URL:
-        process.env.MONGO_URL || 'mongodb+srv://onixjkk_db:qwerty123456@cluster1.wkyectn.mongodb.net/?appName=Cluster1',
+        process.env.MONGO_URL || 'mongodb://localhost:27017/',
+        // 'mongodb+srv://onixjkk_db:qwerty123456@cluster1.wkyectn.mongodb.net/?appName=Cluster1',
     DB_NAME: process.env.DB_NAME || 'blog-platform',
 };
