a89d3f885ec0f9a24f9119d42cbf34969d75a7d0
Alexander <onixjkk@gmail.com>
2026-04-28 21:46:52 +0300

    Refactor delete-blog.handler.ts

--- src/modules/blog/routers/handlers/delete-blog.handler.ts
+++ src/modules/blog/routers/handlers/delete-blog.handler.ts
@@ -2,21 +2,26 @@ import {Request, Response} from 'express';
 import {blogRepository} from "../../repositories/blog.repository";
 import {HttpStatus} from "../../../../core/types/http-statuses";
 import {createErrorsMessages} from "../../../../core/utils/error.utils";
-import {db} from "../../../../db/in-memory.db";
+import {postCollection} from "../../../../db/mongo.db";
 
-export function deleteBlogHandler(req: Request, res: Response) {
-    const id = String(req.params.id);
-    const blog = blogRepository.findById(id);
+export async function deleteBlogHandler(req: Request, res: Response) {
 
-    if (!blog) {
-        res
-            .status(HttpStatus.NotFound_404)
-            .send(createErrorsMessages([{message: "Blog not found", field: "id"}]));
-        return;
+    try {
+        const id = String(req.params.id);
+        const blog = await blogRepository.findById(id);
+
+        if (!blog) {
+            res
+                .status(HttpStatus.NotFound_404)
+                .send(createErrorsMessages([{message: "Blog not found", field: "id"}]));
+            return;
+        }
+
+        await postCollection.deleteMany({blogId: id});
+        await blogRepository.delete(id);
+
+        res.sendStatus(HttpStatus.NoContent_204);
+    } catch (e:unknown) {
+        res.sendStatus(HttpStatus.InternalServerError_500)
     }
-
-    db.posts = db.posts.filter(p => p.blogId !== id);
-    blogRepository.delete(id);
-
-    res.sendStatus(HttpStatus.NoContent_204);
 }
