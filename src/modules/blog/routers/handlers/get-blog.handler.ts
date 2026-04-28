b32bbdde80d871d28ca7f676b7eb4171eb31efb3
Alexander <onixjkk@gmail.com>
2026-04-28 21:46:43 +0300

    Refactor get-blog.handler.ts

--- src/modules/blog/routers/handlers/get-blog.handler.ts
+++ src/modules/blog/routers/handlers/get-blog.handler.ts
@@ -2,17 +2,23 @@ import { Request, Response } from 'express';
 import {blogRepository} from "../../repositories/blog.repository";
 import {HttpStatus} from "../../../../core/types/http-statuses";
 import {createErrorsMessages} from "../../../../core/utils/error.utils";
+import {mapToBlogViewModel} from "../mapers/map-to-blog-view-model.util";
 
-export function getBlogHandler(req: Request, res: Response) {
-    const id = String(req.params.id);
-    const blog = blogRepository.findById(id);
+export async function getBlogHandler(req: Request, res: Response) {
+    try {
+        const id = String(req.params.id);
+        const blog = await blogRepository.findById(id);
 
-    if (!blog) {
-        res
-            .status(HttpStatus.NotFound_404)
-            .send(createErrorsMessages([{message: "Blog not found", field: "id"}]));
-        return;
+        if (!blog) {
+            res
+                .status(HttpStatus.NotFound_404)
+                .send(createErrorsMessages([{message: "Blog not found", field: "id"}]));
+            return;
+        }
+
+        const blogViewModel = mapToBlogViewModel(blog);
+        res.status(HttpStatus.Ok_200).send(blogViewModel);
+    } catch (e:unknown) {
+        res.sendStatus(HttpStatus.InternalServerError_500)
     }
-
-    res.send(blog);
 }
