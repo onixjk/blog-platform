aedfd3da71451170fa6560f887d02f107c9fa26c
Alexander <onixjkk@gmail.com>
2026-04-29 13:07:49 +0300

    Refactoring get-post-list.handler.ts

--- src/modules/post/routers/handlers/get-post-list.handler.ts
+++ src/modules/post/routers/handlers/get-post-list.handler.ts
@@ -1,9 +1,14 @@
 import {Request, Response} from 'express';
 import {postRepository} from "../../repositories/post.repository";
+import {HttpStatus} from "../../../../core/types/http-statuses";
+import {mapToPostViewModel} from "../mapers/map-to-post-view-model.util";
 
-
-export function getPostListHandler(req: Request, res: Response) {
-    const posts = postRepository.findAll();
-
-    res.send(posts);
+export async function getPostListHandler(req: Request, res: Response) {
+    try {
+        const posts = await postRepository.findAll();
+        const postViewModels = posts.map(mapToPostViewModel);
+        res.send(postViewModels);
+    } catch (e: unknown) {
+        res.sendStatus(HttpStatus.InternalServerError_500);
+    }
 }
