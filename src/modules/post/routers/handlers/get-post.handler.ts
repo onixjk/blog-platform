64c76079da8505a731cd7d371582f190d77cef49
Alexander <onixjkk@gmail.com>
2026-04-29 13:07:44 +0300

    Refactoring get-post.handler.ts

--- src/modules/post/routers/handlers/get-post.handler.ts
+++ src/modules/post/routers/handlers/get-post.handler.ts
@@ -2,17 +2,23 @@ import {Request, Response} from 'express';
 import {HttpStatus} from "../../../../core/types/http-statuses";
 import {createErrorsMessages} from "../../../../core/utils/error.utils";
 import {postRepository} from "../../repositories/post.repository";
+import {mapToPostViewModel} from "../mapers/map-to-post-view-model.util";
 
-export function getPostHandler(req: Request, res: Response) {
-    const id = String(req.params.id);
-    const post = postRepository.findById(id);
+export async function getPostHandler(req: Request, res: Response) {
+    try {
+        const id = String(req.params.id);
+        const post = await postRepository.findById(id);
 
-    if (!post) {
-        res
-            .status(HttpStatus.NotFound_404)
-            .send(createErrorsMessages([{message: "Post not found", field: "id"}]));
-        return;
+        if (!post) {
+            res
+                .status(HttpStatus.NotFound_404)
+                .send(createErrorsMessages([{message: "Post not found", field: "id"}]));
+            return;
+        }
+
+        const postViewModel = mapToPostViewModel(post);
+        res.status(HttpStatus.Ok_200).send(postViewModel);
+    } catch (e: unknown) {
+        res.sendStatus(HttpStatus.InternalServerError_500)
     }
-
-    res.send(post);
 }
