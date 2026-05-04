e5619ebeaf008ee706b6772efc2945a4993fc28d
Alexander <onixjkk@gmail.com>
2026-05-04 16:39:53 +0300

    fix

--- src/modules/blog/routers/handlers/update-blog.handler.ts
+++ src/modules/blog/routers/handlers/update-blog.handler.ts
@@ -1,33 +1,18 @@
 import {Request, Response} from 'express';
-import {blogsRepository} from "../../repositories/blogs.repository";
-import {HttpStatus} from "../../../../core/types/http-statuses";
-import {BlogAttributes} from "../../application/dtos/blog-attributes";
-import {postCollection} from "../../../../db/mongo.db";
+import {errorsHandler} from "../../../../core/errors/errors.handler";
+import {blogsService} from "../../application/blogs.service";
+import {BlogUpdateInput} from "../input/blog-update.input";
 
 export async function updateBlogHandler(
-    req: Request<{ id: string }, {}, BlogAttributes>,
+    req: Request<{ id: string }, {}, BlogUpdateInput>,
     res: Response
 ) {
     try {
         const id = req.params.id;
-        const dtoBlogName = req.body.name
-        const blog = await blogsRepository.findById(id);
 
-        // if (!blog) {
-        //     res
-        //         .status(HttpStatus.NotFound_404)
-        //         .send(createErrorsMessages([{message: "Blog not found", field: "id"}]));
-        //     return;
-        // }
+        await blogsService.update(id, req.body.data.attributes);
 
-        await postCollection.updateMany(
-            {blogId: id, blogName: {$ne: dtoBlogName}},
-            {$set: {blogName: dtoBlogName}}
-        );
-
-        await blogsRepository.update(id, req.body);
-        res.sendStatus(HttpStatus.NoContent_204);
     } catch (e: unknown) {
-        res.sendStatus(HttpStatus.InternalServerError_500)
+        errorsHandler(e, res);
     }
 }
